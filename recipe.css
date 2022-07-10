2ddee150918f59a8425c35cd6a94fefc1cedc54c
ogandaeric <98585577+ogandaeric@users.noreply.github.com>
2022-07-10 08:50:42 +0300

    Add files via upload

---
+++ recipe.css
@@ -0,0 +1,63 @@
+body h4{
+    color: #a4a400;
+    text-align: center;
+    background: #F0F0F0;
+    font-size: 22pt;
+    font-family:Lucida Sans, Helvetica, Arial,  Sans-serif ;
+  }
+.section2 h1{
+    color: #a4a400;
+    text-align: left;
+    background: #F0F0F0;
+    font-size: 18pt;
+    font-family:Lucida Sans, Helvetica, Arial,  Sans-serif ;
+}
+.section3 h1{
+    color: #a4a400;
+    text-align: left;
+    background: #F0F0F0;
+    font-size: 18pt;
+    font-family:Lucida Sans, Helvetica, Arial,  Sans-serif ;
+}
+.section4 h1{
+    color: #a4a400;
+    text-align: left;
+    background: #F0F0F0;
+    font-size: 18pt;
+    font-family:Lucida sans-unicode, Helvetica, Arial,  Sans-serif ;
+}
+.section4 p{
+    color: #020202;
+    text-align: left;
+    background: #F0F0F0;
+    background-color: #FFFFA8;   
+    }
+footer h1{
+    color: #a4a400;
+    text-align: left;
+    background: #F0F0F0;
+    font-size: 18pt;
+    font-family:Lucida Sans, Helvetica, Arial,  Sans-serif ;
+}
+button{
+    padding:.1rem 1rem;
+    background:none;
+    border:rgb(243, 239, 239);
+    color:rgb(248, 245, 245);
+    cursor:pointer;
+}
+footer a{
+    color: #a4a400;
+    text-align: left;
+    background: #F0F0F0;
+    font-size: 14pt;
+}
+.section1 ul{
+    list-style-image: url(/images/pie_bullet.png);
+}
+.section2 ul{
+    list-style-image: url(/images/pie_icon.gif);
+}
+.section3 ul{
+    list-style-image: url(/images/pie_icon.gif);
+}
